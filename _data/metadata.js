13bb864b9329e0829f28d5cebe74e9792b4ac02f
Stefánia Péter <1927103+Stefanye@users.noreply.github.com>
2023-03-13 14:26:16 +0100

    Test

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
+	"title": "Stefánia's journal",
+	"url": "https://journal.stef-nia.me/",
 	"language": "en",
 	"description": "I am writing about my experiences as a naval navel-gazer.",
 	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
+		"name": "Stefánia Péter",
+		"email": "hello@stef-nia.me",
+		"url": "https://stef-nia.me"
 	}
 }
